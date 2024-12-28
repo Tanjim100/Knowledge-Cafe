6506853e88c2a8ab3f31bd7add638c8fc89838ca
Tanjim100 <tanjim.haider.12112100@gmail.com>
2024-12-28 16:54:28 +0600

    knowledge cafe blogs connected with json

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+  
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-red-500'>Knowledge cafe</h1>
+      <Blogs></Blogs>
+      
+    </>
+  )
+}
+
+export default App
